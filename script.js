863f6639a6279fd78361d2a718b1baf290b1e226
Aman Saraogi <saraogiaman003@gmail.com>
2023-06-29 16:49:57 +0530

    js done

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-const name = document.querySelector(".notes");
-let notescontainer = document.querySelector(".notesbox");
-function addnote(){
-    notescontainer.appendChild("name");
+const notescontainer = document.querySelector(".notesbox");
+const createbtn = document.querySelector(".btn");
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+  notescontainer.innerHTML = localStorage.getItem("notes");
 }
+showNotes();
+
+function updateStorage() {
+  localStorage.setItem("notes", notescontainer.innerHTML);
+}
+
+createbtn.addEventListener("click", () => {
+  let inputBox = document.createElement("p");
+  let img = document.createElement("img");
+  inputBox.className = "input-box";
+  inputBox.setAttribute("contenteditable", "true");
+  img.src = "images/delete.png";
+  notescontainer.appendChild(inputBox).appendChild(img);
+});
+
+notescontainer.addEventListener("click", function (e) {
+  if (e.target.tagName === "IMG") {
+    e.target.parentElement.remove();
+    updateStorage();
+  } else if ((e.target.tagName = "P")) {
+    notes = document.querySelectorAll(".input-box");
+    notes.forEach((nt) => {
+      nt.onkeyup = function () {
+        updateStorage();
+      };
+    });
+  }
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    document.execCommand("insertLineBreak");
+    event.preventDefault();
+  }
+});
